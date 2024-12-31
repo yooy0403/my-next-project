54c96b1dfb78d480cee686a13dc8eb036386abd5
yo_oy0403 <yosuke.b0403@gmail.com>
2024-12-31 18:56:43 +0900

    7章まで完了

---
+++ app/news/category/[id]/page.tsx
@@ -0,0 +1,34 @@
+import { notFound } from "next/navigation";
+import { getCategoryDetail, getNewsList } from "@/app/_libs/microcms";
+import NewsList from "@/app/_components/NewsList";
+import Pagination from "@/app/_components/Pagination";
+import { NEWS_LIST_LIMIT } from "@/app/_constants";
+
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+export default async function Page({ params }: Props) {
+  const category = await getCategoryDetail(params.id).catch(notFound);
+
+  const { contents: news, totalCount } = await getNewsList({
+    limit: NEWS_LIST_LIMIT,
+    filters: `category[equals]${category.id}`,
+  });
+
+  if (news.length === 0) {
+    notFound();
+  }
+
+  return (
+    <>
+      <NewsList news={news} />
+      <Pagination
+        totalCount={totalCount}
+        basePath={`/news/category/${category.id}`}
+      />
+    </>
+  );
+}
